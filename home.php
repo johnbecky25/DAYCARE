ae5975b7e2fd157ae0c1c5a4911d29168f45d4e2
John Darine <75120528+johnbecky25@users.noreply.github.com>
2023-04-14 23:54:36 +0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,87 @@
+<?php
+include('includes/config.php');
+session_start();
+error_reporting(0);
+?>
+<!DOCTYPE html>
+<html lang="zxx">
+   <head>
+      <title>Blessings Daycare</title>
+      <!--meta tags -->
+      <script>
+         addEventListener("load", function () {
+         	setTimeout(hideURLbar, 0);
+         }, false);
+         function hideURLbar() {
+     	window.scrollTo(0, 1);
+     }
+  </script>
+  <!--booststrap-->
+  <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+  <!--//booststrap end-->
+  <!-- font-awesome icons -->
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <!-- //font-awesome icons -->
+  <!--stylesheets-->
+  <link href="css/style.css" rel='stylesheet' type='text/css' media="all">
+  <!--//stylesheets-->
+  <link href="//fonts.googleapis.com/css?family=Dosis:400,500,600,700" rel="stylesheet">
+  <link href="//fonts.googleapis.com/css?family=Quicksand:400,500,700" rel="stylesheet">
+ <!-- 
+  <style>
+    @keyframes slidy {
+0% { left: 0%; }
+20% { left: 0%; }
+25% { left: -100%; }
+45% { left: -100%; }
+50% { left: -200%; }
+70% { left: -200%; }
+75% { left: -300%; }
+95% { left: -300%; }
+100% { left: -400%; }
+}
+
+body { margin: 0; } 
+
+div#slider { overflow: hidden; }
+div#slider figure img { width: 20%; float: left; }
+div#slider figure { 
+  position: relative;
+  width: 500%;
+  margin: 0;
+  left: 0;
+  text-align: left;
+  font-size: 0;
+  animation: 30s slidy infinite; 
+} 
+  </style>
+-->
+   </head>
+   <body>
+      <div class="main-top" id="home">
+         <!-- header -->
+         <?php include_once("includes/navbar.php");?>
+         <!-- //header -->
+         <!-- banner -->
+      
+<div class="banner">
+   <img src="slider/baby2.jpg" alt="Banner Image" style="width:100%; height:auto;">
+</div>
+         <!--// banner -->
+              <!-- about -->
+
+     <!--//about -->	  
+
+     <!--//about -->
+
+     <!-- team -->
+
+     <!--//team -->
+
+     <!--//contact -->
+     <!-- footer -->
+
+     <!--//footer -->
+  </div>
+  </body>
+</html>
